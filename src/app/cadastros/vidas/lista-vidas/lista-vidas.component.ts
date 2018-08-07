ed8f4e63cf79d840cb11f35b4dacdce77256ed55
naiade <naiade.lima@live.com>
2018-08-07 14:56:20 -0300

    vidas

--- src/app/cadastros/vidas/lista-vidas/lista-vidas.component.ts
+++ src/app/cadastros/vidas/lista-vidas/lista-vidas.component.ts
@@ -1,13 +1,25 @@
-import {Component, ViewChild} from '@angular/core';
-import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import { Component, ViewChild } from "@angular/core";
+import { SelectionModel } from "@angular/cdk/collections";
+import { MatPaginator, MatSort, MatTableDataSource } from "@angular/material";
+
 @Component({
-  selector: 'app-mix',
-  templateUrl: './lista-vidas.component.html',
-  styleUrls: ['./lista-vidas.component.scss']
+  selector: "app-mix",
+  templateUrl: "./lista-vidas.component.html",
+  styleUrls: ["./lista-vidas.component.scss"]
 })
 export class ListaVidasComponent {
-   displayedColumns = ['id', 'nome', 'dt_nasc', 'dt_adm', 'dt_dem', 'cpf', 'nitpis'];
+  displayedColumns = [
+    "select",
+    "id",
+    "nome",
+    "dt_nasc",
+    "dt_adm",
+    "dt_dem",
+    "cpf",
+    "nitpis"
+  ];
   dataSource: MatTableDataSource<UserData>;
+  selection = new SelectionModel<UserData>(true, []);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -15,12 +27,27 @@ export class ListaVidasComponent {
   constructor() {
     // Create 100 users
     const users: UserData[] = [];
-    for (let i = 1; i <= 100; i++) { users.push(createNewUser(i)); }
+    for (let i = 1; i <= 100; i++) {
+      users.push(createNewUser(i));
+    }
 
     // Assign the data to the data source for the table to render
     this.dataSource = new MatTableDataSource(users);
   }
 
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    debugger;
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.dataSource.data.forEach(row => this.selection.select(row));
+  }
   /**
    * Set the paginator and sort after the view init since this component will
    * be able to query its view for the initialized paginator and sort.
@@ -39,8 +66,10 @@ export class ListaVidasComponent {
 /** Builds and returns a new User. */
 function createNewUser(id: number): UserData {
   const name =
-  NOMES[Math.round(Math.random() * (NOMES.length - 1))] + ' ' +
-  NOMES[Math.round(Math.random() * (NOMES.length - 1))].charAt(0) + '.';
+    NOMES[Math.round(Math.random() * (NOMES.length - 1))] +
+    " " +
+    NOMES[Math.round(Math.random() * (NOMES.length - 1))].charAt(0) +
+    ".";
 
   return {
     id: id.toString(),
@@ -48,17 +77,24 @@ function createNewUser(id: number): UserData {
     dt_nasc: DT_NASC[Math.round(Math.random() * (DT_NASC.length - 1))],
     dt_adm: DT_ADM[Math.round(Math.random() * (DT_ADM.length - 1))],
     dt_dem: null,
-    cpf:CPF[Math.round(Math.random() * (CPF.length - 1))],
+    cpf: CPF[Math.round(Math.random() * (CPF.length - 1))],
     nitpis: PIS[Math.round(Math.random() * (PIS.length - 1))]
   };
 }
 
 /** Constants used to fill up our data base. */
-const DT_NASC = ['01/01/1970', '01/01/1990'];
-const CPF = ['084.687.557-88', '402.757.028-63'];
-const PIS = ['123456789', '123456755'];
-const DT_ADM = ['03/06/2014', '03/11/2014'];
-const NOMES = ['José Bonifácio', 'Wescley Sudré', 'Naiade Lima', 'Augusto Pessoa', 'Rosa Almeira', 'Jack'];
+const DT_NASC = ["01/01/1970", "01/01/1990"];
+const CPF = ["084.687.557-88", "402.757.028-63"];
+const PIS = ["123456789", "123456755"];
+const DT_ADM = ["03/06/2014", "03/11/2014"];
+const NOMES = [
+  "José Bonifácio",
+  "Wescley Sudré",
+  "Naiade Lima",
+  "Augusto Pessoa",
+  "Rosa Almeira",
+  "Jack"
+];
 
 export interface UserData {
   id: string;
@@ -68,5 +104,4 @@ export interface UserData {
   dt_dem: string;
   cpf: string;
   nitpis: string;
-
 }
